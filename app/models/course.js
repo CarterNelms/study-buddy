cf6f17798124cf27470733d71d37e2840e534e94
CarterNelms <carternelms@bellsouth.net>
2014-05-23 12:38:28 -0500

    init teacherBackend

--- app/models/course.js
+++ app/models/course.js
@@ -3,3 +3,16 @@
 //name
 //subject
 //description
+
+class Course
+{
+  constructor(obj)
+  {
+    this.userId = obj.userId;
+    this.name = obj.name;
+    this.subject = obj.subject;
+    this.description = obj.description;
+  }
+}
+
+module.exports = Course;
