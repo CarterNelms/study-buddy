058d3c6356647f2601e21e66306fb431f9a402cd
Seth March <seth.d.march@gmail.com>
2014-05-23 14:28:56 -0500

    create user, login encryption

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,13 +1,25 @@
 'use strict';
 
-exports.index = (req, res)=>{
-  res.render('home/index', {title: 'Node.js: Home'});
-};
+var traceur = require('traceur');
+var User = traceur.require(__dirname + '/../models/user.js');
 
 exports.create = (req, res)=>{
-  console.log(req.body);
+  var user = new User(req.body);
+  user.create(user=>{
+    console.log(user);
+    res.redirect('/portal');
+  });
 };
 
 exports.login = (req, res)=>{
-  console.log(req.query);
+  var user = new User(req.query);
+  user.login(u=>{
+    if(u){
+      req.session.userId = u._id;
+      res.redirect('/user');
+    }else{
+      req.session.userId = null;
+      res.redirect('/portal');
+    }
+  });
 };
