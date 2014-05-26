6b0c624880910ae6927442c9ef6b7eaf2a95e108
Terrence <terrenceljones805@gmail.com>
2014-05-26 15:18:23 -0500

    added conditional links to home page for 'teach' and 'learn'

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,8 @@
 'use strict';
 
 exports.index = (req, res)=>{
-  res.render('home/index', {title: 'Node.js: Home'});
+  var userId = req.session.userId;
+  res.render('home/index', {userId: userId, title: 'Node.js: Home'});
 };
 exports.about = (req,res)=>{
   res.render('home/about', {title: 'About Page'});
