aca7c8af5386f37cd2b56004148a34a45d91960d
CarterNelms <carternelms@bellsouth.net>
2014-05-25 19:00:27 -0500

    added js and a hidden input to new lesson page (maybe should have been done in frontend branch. No conflicts) git push origin teacherBackend

---
+++ app/static/js/es6/lessonNew.es6.js
@@ -0,0 +1,17 @@
+'use strict';
+
+$(function()
+{
+  var $form = $('form.lesson');
+  var $button = $form.find('button');
+  $button.click(submitNewLesson);
+
+  function submitNewLesson(e)
+  {
+    $(this).closest('form').submit(function()
+    {
+      var html = $('#content-container iframe').contents().find('body').html();
+      $('input[name=material]').val(html);
+    });
+  }
+});
