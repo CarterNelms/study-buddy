7167e1a5b22a9fe0c317bf103c14f7fd54ce876f
Terrence <terrenceljones805@gmail.com>
2014-05-26 23:36:46 -0500

    added previously created questions to test edit page

--- app/routes/tests.js
+++ app/routes/tests.js
@@ -38,7 +38,7 @@ exports.create = (req, res)=>
 
 exports.prepEdit = (req, res)=>{
   var userId = req.session.userId;
-  res.render('user/test', {userId: userId, title: 'Create a Test'});
+  res.render('tests/edit', {userId: userId, title: 'Edit a Test'});
 };
 exports.Edit = (req,res)=>{
   res.redirect('/user/courses/:coursesId');
