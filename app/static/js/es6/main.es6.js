df45ac653f998472f4fe9c27061a080bf645d481
Terrence <terrenceljones805@gmail.com>
2014-05-27 00:18:17 -0500

    added question and answers array to students test page

--- app/static/js/es6/main.es6.js
+++ app/static/js/es6/main.es6.js
@@ -1,15 +1,11 @@
-// (function(){
-//   'use strict';
-//
-//   $(document).ready(init);
-//
-//   function init(){
-//     $('.add-test-question').on('click', addQuestion);
-//   }
-//
-//   function addQuestion(event){
-//     var question = $('form#test .question-answer-container:first');
-//     $('form#test').append(question.clone()).find('.question-answer-container:last-child input').val('');
-//     event.preventDefault();
-//   }
-// })();
+(function(){
+  'use strict';
+
+  $(document).ready(init);
+
+  function init(){
+    $('.add-test-question').on('click', addQuestion);
+  }
+
+
+})();
